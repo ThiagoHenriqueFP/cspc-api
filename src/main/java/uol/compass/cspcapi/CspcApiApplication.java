700ec93e2649483845cf895f1f4c83eb08b658ff
Thiago Henrique <thiagohp070@gmail.com>
2023-07-24 15:56:36 -0300

    git init

---
+++ src/main/java/uol/compass/cspcapi/CspcApiApplication.java
@@ -0,0 +1,13 @@
+package uol.compass.cspcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CspcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CspcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uol/compass/cspcapi/CspcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uol.compass.cspcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CspcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
