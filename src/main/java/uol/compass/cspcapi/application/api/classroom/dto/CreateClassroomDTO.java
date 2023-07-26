b9ab417ed1017c7ac10a1663c02c2608ec2f1f45
Gabriel Rodrigues P. Rocha <gabrielrodrigues1927@gmail.com>
2023-07-26 11:39:30 -0300

    create DTOclassrooom

---
+++ src/main/java/uol/compass/cspcapi/application/api/classroom/dto/CreateClassroomDTO.java
@@ -0,0 +1,40 @@
+package uol.compass.cspcapi.application.api.classroom.dto;
+
+import uol.compass.cspcapi.domain.Squad.Squad;
+import uol.compass.cspcapi.domain.coordinator.Coordinator;
+import uol.compass.cspcapi.domain.scrumMaster.ScrumMaster;
+import uol.compass.cspcapi.domain.student.Student;
+
+import java.util.List;
+
+public class CreateClassroomDTO {
+
+    private String title;
+    private Coordinator coordinator;
+
+    private List<Student> students;
+
+    private List<ScrumMaster> scrumMasters;
+
+    private List<Squad> squads;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Coordinator getCoordinator() {
+        return coordinator;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public List<ScrumMaster> getScrumMasters() {
+        return scrumMasters;
+    }
+
+    public List<Squad> getSquads() {
+        return squads;
+    }
+}
