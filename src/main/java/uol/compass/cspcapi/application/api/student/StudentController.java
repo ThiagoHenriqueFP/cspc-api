e51219dabfed4c47b1267f56a250751ab1fac970
Thiago Henrique <thiagohp070@gmail.com>
2023-07-25 22:23:42 -0300

    feat: implement student post method

--- src/main/java/uol/compass/cspcapi/application/api/student/StudentController.java
+++ src/main/java/uol/compass/cspcapi/application/api/student/StudentController.java
@@ -2,13 +2,14 @@ package uol.compass.cspcapi.application.api.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import uol.compass.cspcapi.application.api.student.dto.CreateStudentDTO;
 import uol.compass.cspcapi.domain.student.Student;
 import uol.compass.cspcapi.domain.student.StudentRepository;
 import uol.compass.cspcapi.domain.student.StudentService;
 import uol.compass.cspcapi.domain.user.UserService;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("students")
 public class StudentController {
     private StudentService studentService;
 
@@ -19,7 +20,7 @@ public class StudentController {
 
     @PostMapping
     public Student save(
-            @RequestBody Student student
+            @RequestBody CreateStudentDTO student
     ){
         return studentService.save(student);
     }
