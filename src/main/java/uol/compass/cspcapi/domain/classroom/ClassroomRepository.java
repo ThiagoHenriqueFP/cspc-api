257a8eeac8a35c8c8d0a470c98608ec81c4eaf31
Gabriel Rodrigues P. Rocha <gabrielrodrigues1927@gmail.com>
2023-07-26 19:29:54 -0300

    add others met in repository class

--- src/main/java/uol/compass/cspcapi/domain/classroom/ClassroomRepository.java
+++ src/main/java/uol/compass/cspcapi/domain/classroom/ClassroomRepository.java
@@ -2,11 +2,19 @@ package uol.compass.cspcapi.domain.classroom;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClassroomRepository extends JpaRepository<Classrooms, Long> {
 
     Optional<Classrooms> findById(Long id);
+    List<Classrooms> findAll();
 
-    Classrooms deleteById(Classrooms classrooms);
+    void deleteById(Long id);
+
+    List<Classrooms> findAllBySomeAttribute(String attribute);
+
+    void deleteAll();
+
+
 }
