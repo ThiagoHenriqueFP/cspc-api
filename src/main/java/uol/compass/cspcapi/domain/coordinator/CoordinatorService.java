28b333de4ca50de7d5d15060ae47f84dcdc8c13e
Gabriel Rodrigues P. Rocha <gabrielrodrigues1927@gmail.com>
2023-07-26 18:50:54 -0300

    add Method in service

--- src/main/java/uol/compass/cspcapi/domain/coordinator/CoordinatorService.java
+++ src/main/java/uol/compass/cspcapi/domain/coordinator/CoordinatorService.java
@@ -1,4 +1,22 @@
 package uol.compass.cspcapi.domain.coordinator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
 public class CoordinatorService {
+
+    private CoordinatorRepository coordinatorRepository;
+
+    @Autowired
+    public CoordinatorService(CoordinatorRepository coordinatorRepository) {
+        this.coordinatorRepository = coordinatorRepository;
+    }
+
+    public Coordinator getCoordinatorById(Long coordinatorId) {
+        return coordinatorRepository.findById(coordinatorId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "coordinator not found"));
+    }
 }
