1396f921cf3af6f75bcb0082f625e51e0f7bde89
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2023-07-30 16:46:09 -0300

    add method saveScrumMasterSucess in ScrumMasterServiceTest

--- src/test/java/uol/compass/cspcapi/ScrumMasterServiceTest.java
+++ src/test/java/uol/compass/cspcapi/ScrumMasterServiceTest.java
@@ -3,14 +3,19 @@ package uol.compass.cspcapi;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import uol.compass.cspcapi.application.api.scrumMaster.dto.CreateScrumMasterDTO;
+import uol.compass.cspcapi.application.api.scrumMaster.dto.ResponseScrumMasterDTO;
 import uol.compass.cspcapi.domain.scrumMaster.ScrumMasterRepository;
 import uol.compass.cspcapi.domain.scrumMaster.ScrumMasterService;
+import uol.compass.cspcapi.domain.user.User;
 import uol.compass.cspcapi.domain.user.UserRepository;
 import uol.compass.cspcapi.domain.user.UserService;
 import uol.compass.cspcapi.infrastructure.config.passwordEncrypt.PasswordEncrypt;
@@ -47,6 +52,16 @@ public class ScrumMasterServiceTest {
     	}
     }
 	
-
+	@Test
+	public void saveScrumMasterSucess(){
+		User user = new User("userTest", "test", "test@mail", "JHHGF1");
+		CreateScrumMasterDTO scrumMaster = new CreateScrumMasterDTO(user);
+		ResponseScrumMasterDTO response = scrumMasterService.save(scrumMaster);
+		if(response != null) {
+			idList.add(response.getId());
+		}
+		
+		Assertions.assertThat(response).isNotEqualTo(null);
+	}
 
 }
