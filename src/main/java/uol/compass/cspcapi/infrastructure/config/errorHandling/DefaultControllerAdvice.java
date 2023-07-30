9529e39096df401ad2e8df24e61d85839407087e
Thiago Henrique <thiagohp070@gmail.com>
2023-07-30 17:17:05 -0300

    feat: custom exception handling

--- src/main/java/uol/compass/cspcapi/infrastructure/config/errorHandling/DefaultControllerAdvice.java
+++ src/main/java/uol/compass/cspcapi/infrastructure/config/errorHandling/DefaultControllerAdvice.java
@@ -8,27 +8,53 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import uol.compass.cspcapi.application.api.generalPourposeDTO.ResponseDTO;
 
-import java.util.List;
+import javax.naming.AuthenticationException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class DefaultControllerAdvice extends ResponseEntityExceptionHandler {
+public class DefaultControllerAdvice {
     @ExceptionHandler(RuntimeException.class)
-    protected ResponseEntity<Object> handlerMethodArgumentNotValid(
-            MethodArgumentNotValidException ex,
-            HttpHeaders headers,
-            HttpStatus status,
-            WebRequest request
+    protected ResponseEntity<Object> handlerRuntime(
+            RuntimeException ex
     ){
-        List<String> messages = ex.getBindingResult()
+
+        ResponseDTO<String> responseDTO = new ResponseDTO<>(ex.getMessage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    protected ResponseEntity<Object> handlerAuthentication(
+            AuthenticationException ex
+    ){
+        ResponseDTO<String> responseDTO = new ResponseDTO<>(ex.getMessage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex
+    ) {
+
+        List<String> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .toList();
 
-        ResponseDTO<?> responseDTO = new ResponseDTO<>(messages);
-        return new ResponseEntity<>(responseDTO, status);
+        ResponseDTO<List<String>> responseDTO = new ResponseDTO<>(errors);
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    protected ResponseEntity<Object> handlerResponseStatus(
+            ResponseStatusException ex
+    ){
+        ResponseDTO<?> responseDTO = new ResponseDTO<>(ex.getMessage());
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 }
