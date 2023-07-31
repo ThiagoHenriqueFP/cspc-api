07ac80e33ef082074262ed68062b8107937cb686
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2023-07-30 21:25:14 -0300

    Update ScrumMasterServiceTest.java

--- src/test/java/uol/compass/cspcapi/ScrumMasterServiceTest.java
+++ src/test/java/uol/compass/cspcapi/ScrumMasterServiceTest.java
@@ -22,47 +22,64 @@ import uol.compass.cspcapi.infrastructure.config.passwordEncrypt.PasswordEncrypt
 
 @SpringBootTest
 public class ScrumMasterServiceTest {
-	
-	
+
 	private ScrumMasterService scrumMasterService;
 	@Autowired
 	private ScrumMasterRepository scrumMasterRepository;
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
-    private UserService userService;
+	private UserService userService;
 	@Autowired
-    private PasswordEncrypt passwordEncrypt;
-    private PasswordEncoder passwordEncoder;
-    private List<Long> idList;
-    
-    @BeforeEach
+	private PasswordEncrypt passwordEncrypt;
+	private PasswordEncoder passwordEncoder;
+	private List<Long> idList;
+
+	@BeforeEach
 	public void setup() {
-    	userService = new UserService(userRepository, passwordEncoder);
+		userService = new UserService(userRepository, passwordEncoder);
 		scrumMasterService = new ScrumMasterService(scrumMasterRepository, userService, passwordEncrypt);
 		idList = new ArrayList<>();
 	}
-    
-    @AfterEach
-    public void clean() {
-    	for (int i = 0; i < idList.size(); i++) {
-    		if (!idList.isEmpty()) {
-    			scrumMasterService.delete(idList.get(i));
-    			userRepository.deleteById(idList.get(i));
-    		}
-    	}
-    }
-	
+
+	@AfterEach
+	public void clean() {
+		for (int i = 0; i < idList.size(); i++) {
+			if (!idList.isEmpty()) {
+				scrumMasterService.delete(idList.get(i));
+				userRepository.deleteById(idList.get(i));
+			}
+		}
+		userRepository.deleteAll();
+		scrumMasterRepository.deleteAll();
+	}
+
 	@Test
-	public void saveScrumMasterSucess(){
-		User user = new User("userTest", "test", "test@mail", "JHHGF1");
+	public void saveScrumMasterSucess() {
+		User user = new User("user2Test", "test", "test2@mail", "JHHGF1");
 		CreateScrumMasterDTO scrumMaster = new CreateScrumMasterDTO(user);
 		ResponseScrumMasterDTO response = scrumMasterService.save(scrumMaster);
-		if(response != null) {
+		if (response != null) {
 			idList.add(response.getId());
 		}
-		
 		Assertions.assertThat(response).isNotEqualTo(null);
 	}
+
+//	@Test
+//	public void saveScrumMasterFailureFieldClean() {
+//		User user = new User("userTest3", "", "test3@mail", "");
+//		CreateScrumMasterDTO scrumMaster = new CreateScrumMasterDTO(user);
+//		ResponseScrumMasterDTO response = null;
+//		boolean error = false;
+//		try {
+//			response = scrumMasterService.save(scrumMaster);
+//		
+//		} catch (ResponseStatusException e) {
+//			error = true;
+//		}
+//		
+//
+//		Assertions.assertThat(error).isEqualTo(true);
+//	}
 
 }
