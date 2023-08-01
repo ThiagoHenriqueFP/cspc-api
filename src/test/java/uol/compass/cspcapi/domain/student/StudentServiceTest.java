185955d1e732dd03872dc5efdd9a3ec9d888ba11
Thiago Henrique <thiagohp070@gmail.com>
2023-07-31 21:28:37 -0300

    feat: StudentServiceTest basic tests

--- src/test/java/uol/compass/cspcapi/domain/student/StudentServiceTest.java
+++ src/test/java/uol/compass/cspcapi/domain/student/StudentServiceTest.java
@@ -8,7 +8,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.web.server.ResponseStatusException;
 import uol.compass.cspcapi.application.api.student.dto.CreateStudentDTO;
 import uol.compass.cspcapi.application.api.student.dto.ResponseStudentDTO;
+import uol.compass.cspcapi.application.api.student.dto.UpdateStudentDTO;
 import uol.compass.cspcapi.application.api.user.dto.CreateUserDTO;
+import uol.compass.cspcapi.application.api.user.dto.UpdateUserDTO;
 import uol.compass.cspcapi.domain.role.Role;
 import uol.compass.cspcapi.domain.role.RoleRepository;
 import uol.compass.cspcapi.domain.role.RoleService;
@@ -17,6 +19,8 @@ import uol.compass.cspcapi.domain.user.UserRepository;
 import uol.compass.cspcapi.domain.user.UserService;
 import uol.compass.cspcapi.infrastructure.config.passwordEncrypt.PasswordEncoder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,6 +37,7 @@ class StudentServiceTest {
     private static final Role ROLE_1 = new Role("ROLE_STUDENT");
 
     private static final Student STUDENT_1 = new Student(USER_1);
+    private static final Student STUDENT_2 = new Student(USER_1);
 
 
     @MockBean
@@ -71,6 +76,8 @@ class StudentServiceTest {
         studentService = new StudentService(studentRepository, userService, new PasswordEncoder(), roleService);
 
         USER_1.getRoles().add(roleService.findRoleByName("ROLE_STUDENT"));
+        STUDENT_1.setId(1L);
+        STUDENT_2.setId(2L);
     }
 
     @AfterEach
@@ -112,32 +119,163 @@ class StudentServiceTest {
         assertEquals("teste@mail.com", student.getUser().getEmail());
     }
 
-//    @Test
-//    void getByIdFail(){
-//        when(studentRepository.findById(1L)).thenThrow(ResponseStatusException.class);
-//
-//        Exception response = assertThrows(Exception.class,
-//                () -> studentService.getById(1L)
-//        );
-//
-//        assertEquals(404, response.getStatusCode().value());
-//        assertEquals("user not found", response.getMessage());
-//    }
+    @Test
+    void getByIdFail(){
+        when(studentRepository.findById(1L)).thenThrow(ResponseStatusException.class);
+
+        ResponseStatusException response = assertThrows(ResponseStatusException.class,
+                () -> studentService.getById(0L)
+        );
+
+        assertEquals(404, response.getStatusCode().value());
+        assertEquals("user not found", response.getReason());
+    }
 
     @Test
     void getAll() {
+        List<Student> list = new ArrayList<>();
+        list.add(STUDENT_1);
+        list.add(STUDENT_2);
+
+        when(studentRepository.findAll()).thenReturn(list);
+
+        List<ResponseStudentDTO> response = studentService.getAll();
+
+        assertEquals(list.size(), response.size());
+    }
+
+    @Test
+    void getAllIncorrectSize() {
+        List<Student> list = new ArrayList<>();
+        list.add(STUDENT_1);
+
+        when(studentRepository.findAll()).thenReturn(list);
+
+        List<ResponseStudentDTO> response = studentService.getAll();
+
+        list.remove(STUDENT_1);
+
+        assertNotEquals(list.size(), response.size());
+        assertEquals(0, list.size());
+        assertEquals(1, response.size());
     }
 
     @Test
     void update() {
+        Student student = new Student(
+                STUDENT_1.getUser()
+        );
+
+        student.getUser().setEmail("testePut@mail.com");
+
+        when(studentRepository.findById(anyLong())).thenReturn(Optional.of(STUDENT_1));
+        when(studentRepository.save(any())).thenReturn(student);
+
+        ResponseStudentDTO updatedStudent = studentService.update(1L, new UpdateStudentDTO(
+                new UpdateUserDTO(
+                        USER_1.getFirstName(),
+                        USER_1.getLastName(),
+                        USER_1.getEmail(),
+                        USER_1.getPassword()
+                )
+        ));
+
+        assertEquals(student.getUser().getEmail(), updatedStudent.getUser().getEmail());
+    }
+
+    @Test
+    void updateFailNotFound(){
+        when(studentRepository.findById(1L)).thenReturn(Optional.of(STUDENT_1)).thenThrow(ResponseStatusException.class);
+
+        Student student = new Student(
+                STUDENT_1.getUser()
+        );
+
+        student.getUser().setEmail("testePut@mail.com");
+
+
+        ResponseStatusException response = assertThrows(ResponseStatusException.class,
+                () -> studentService.update(0L, new UpdateStudentDTO(
+                new UpdateUserDTO(
+                        USER_1.getFirstName(),
+                        USER_1.getLastName(),
+                        USER_1.getEmail(),
+                        USER_1.getPassword()
+                )
+        )));
+
+        assertEquals(404, response.getStatusCode().value());
+        assertEquals("student not found", response.getReason());
     }
 
     @Test
     void delete() {
+       when(studentRepository.findById(anyLong())).thenReturn(Optional.of(STUDENT_1));
+
+       studentService.delete(anyLong());
+
+       verify(studentRepository).delete(STUDENT_1);
+    }
+
+    @Test
+    void deleteFailNotFound() {
+        when(studentRepository.findById(1L)).thenThrow(ResponseStatusException.class);
+
+        ResponseStatusException response = assertThrows(ResponseStatusException.class,
+                () -> studentService.delete(anyLong())
+        );
+
+        assertEquals(404, response.getStatusCode().value());
+        assertEquals("student not found", response.getReason());
     }
 
     @Test
     void getAllStudentsById() {
+        List<Long> list = new ArrayList<>();
+        list.add(1L);
+        list.add(2L);
+
+        when(studentRepository.findAllByIdIn(list)).thenReturn(List.of(STUDENT_1, STUDENT_2));
+
+        List<Student> response = studentService.getAllStudentsById(list);
+
+        assertEquals(list.size(), response.size());
+        assertEquals(list.get(0), response.get(0).getId());
+    }
+
+
+
+    @Test
+    void getAllByIdFail(){
+        List<Long> list = new ArrayList<>();
+        list.add(2L);
+        list.add(1L);
+
+        when(studentRepository.findAllByIdIn(list)).thenReturn(List.of(STUDENT_1, STUDENT_2));
+
+        List<Student> response = studentService.getAllStudentsById(list);
+
+        assertEquals(list.size(), response.size());
+        assertNotEquals(list.get(1), response.get(1).getId());
+        assertNotEquals(list.get(0), response.get(0).getId());
+    }
+
+    @Test
+    void getAllByIdFailNotFound(){
+        List<Long> list = new ArrayList<>();
+        list.add(1L);
+        list.add(2L);
+
+        when(studentRepository.findAllByIdIn(list)).thenReturn(List.of(STUDENT_1, STUDENT_2));
+        when(studentRepository.findById(0L)).thenThrow(ResponseStatusException.class);
+
+        List<Student> students = studentService.getAllStudentsById(list);
+
+        ResponseStatusException response = assertThrows(ResponseStatusException.class,
+                () -> students.forEach( it -> studentService.getById(it.getId()))
+        );
+
+        assertEquals(404, response.getStatusCode().value());
     }
 
     @Test
